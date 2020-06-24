0b5b3d73fe0dd38777210bfc9d014f7734c5c17f
ChubaOraka <ChubaOraka@users.noreply.github.com>
2020-06-24 12:58:57 -0400

    :art: Changed donate template

--- www/themes/codespa/template-donate.php
+++ www/themes/codespa/template-donate.php
@@ -14,8 +14,72 @@
     <div class="row">
       <div class="col-sm-8">
         <h2>Donate Today!</h2>
-        <?php  gravity_form( 'Donate Form', false, false, false, '', true );  ?>
-        <?php  wx_build_embed("https://nest.givingfuel.com/nest-online-donations", "Click Here") ;  ?>
+        <?php /* gravity_form( 'Donate Form', false, false, false, '', true );  ?>
+        <?php  wx_build_embed("https://nest.givingfuel.com/nest-online-donations", "Click Here") ; */ ?>
+
+        <!-- <img class="col-sm-4 img-circle" src="https://placehold.it/75">
+        <div class="col-xs-1"></div>
+        <img class="col-sm-4 img-circle" src="https://placehold.it/75"> -->
+        
+        <section class="section-padded">
+          <div class="row vertical-align">
+              <div class="col-sm-6">
+                <h3>Give Online</h2>
+                <p>Donate securely online on <a href='https://codespa-america.givingfuel.com/codespa-america-donation-link'>our Giving Fuel Platform</a>.</p>
+              </div>
+              <div class="col-sm-6">
+                <div class="row vertical-align-mobile">
+                  <div class="col-xs-1"></div>
+                  <!-- <div class="col-xs-6">
+                    <a tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="Around the world, there are millions of stories of men, women and young people who face immense challenges" data-original-title="Their Stories"><img class="codespa-logo" src="http://localhost/wp-content/themes/codespa/assets/images/stories.png"></a>
+                  </div> -->
+                  <div class="col-xs-8">
+                    <a href="https://codespa-america.givingfuel.com/codespa-america-donation-link" class="orbit orbit-right" tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="'For local families, the change is our the ability to pay for animal feed, for good seed, for school materials and school for the children, and food for ourselves. That is the impact.'" data-original-title="Luciano (Angola)" target="_blank"><img src="https://placehold.it/287"></a>
+                    <!-- <a class="orbit orbit-right" tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="'For local families, the change is our the ability to pay for animal feed, for good seed, for school materials and school for the children, and food for ourselves. That is the impact.'" data-original-title="Luciano (Angola)"><img src="http://localhost/wp-content/themes/codespa/assets/images/angola-luciano.jpg"></a> -->
+                    <!-- <a class="orbit orbit-right" tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="'We lend money to people we know are responsible and to our neighbors in other communities, we tell them that nothing is impossible when you are honest and work hard.'" data-original-title="Rosa (Colombia)"><img src="http://localhost/wp-content/themes/codespa/assets/images/columbia-rosa.jpg"></a> -->
+                    <!-- <a class="orbit orbit-right" tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="'I would like to further extend my activity ... I would like to grow my business now that I no longer have space. I have so much demand but no space to put my new workers.'" data-original-title="Maman Eugenie (Republic of Congo)"><img src="http://localhost/wp-content/themes/codespa/assets/images/congo-maman.jpg"></a> -->
+                  </div>
+                  <div class="col-xs-1"></div>
+                </div>
+              </div>
+          </div>
+        </section>
+
+        <section class="section-padded">
+          <div class="row vertical-align">
+            <div class="col-sm-6">
+              <h3>Mail</h2>
+              <p>Make checks payable and send to "CODESPA America" and mail to our address at:</p>
+              <p itemscope itemtype="http://schema.org/PostalAddress">
+                 <span itemprop="name">CODESPA America</span><br>
+                 <span itemprop="streetAddress">1050 Connecticut Ave NW #66235</span><br>
+                 <span itemprop="addressLocality">Washington</span>,
+                 <span itemprop="addressRegion">DC</span>
+                 <span itemprop="postalCode">20035</span><br>
+                 <span itemprop="telephone">(202) 204-3017</span><br>
+              </p>
+              <p>
+                 <span itemprop="email">CODESPA America is able to accept donations via direct deposit or wire transfer. Please send a mail to <a href="mailto:&#105;&#110;&#102;&#111;&#064;&#099;&#111;&#100;&#101;&#115;&#112;&#097;&#045;&#097;&#109;&#101;&#114;&#105;&#099;&#097;&#046;&#111;&#114;&#103;">&#105;&#110;&#102;&#111;&#064;&#099;&#111;&#100;&#101;&#115;&#112;&#097;&#045;&#097;&#109;&#101;&#114;&#105;&#099;&#097;&#046;&#111;&#114;&#103;</a> for enquiries.</span>
+              </p>
+            </div>
+            <div class="col-sm-6">
+              <div class="row vertical-align-mobile">
+                <div class="col-xs-1"></div>
+                <!-- <div class="col-xs-6">
+                  <a tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="Around the world, there are millions of stories of men, women and young people who face immense challenges" data-original-title="Their Stories"><img class="codespa-logo" src="http://localhost/wp-content/themes/codespa/assets/images/stories.png"></a>
+                </div> -->
+                <div class="col-xs-8">
+                  <a class="orbit orbit-right" tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="'For local families, the change is our the ability to pay for animal feed, for good seed, for school materials and school for the children, and food for ourselves. That is the impact.'" data-original-title="Luciano (Angola)"><img src="https://placehold.it/287"></a>
+                  <!-- <a class="orbit orbit-right" tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="'For local families, the change is our the ability to pay for animal feed, for good seed, for school materials and school for the children, and food for ourselves. That is the impact.'" data-original-title="Luciano (Angola)"><img src="http://localhost/wp-content/themes/codespa/assets/images/angola-luciano.jpg"></a> -->
+                  <!-- <a class="orbit orbit-right" tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="'We lend money to people we know are responsible and to our neighbors in other communities, we tell them that nothing is impossible when you are honest and work hard.'" data-original-title="Rosa (Colombia)"><img src="http://localhost/wp-content/themes/codespa/assets/images/columbia-rosa.jpg"></a> -->
+                  <!-- <a class="orbit orbit-right" tabindex="0" role="button" data-toggle="popover" data-container="body" data-trigger="focus hover" data-placement="top" title="" data-content="'I would like to further extend my activity ... I would like to grow my business now that I no longer have space. I have so much demand but no space to put my new workers.'" data-original-title="Maman Eugenie (Republic of Congo)"><img src="http://localhost/wp-content/themes/codespa/assets/images/congo-maman.jpg"></a> -->
+                </div>
+                <div class="col-xs-1"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+
       </div>
       <div class="col-sm-4">
         <div class="row">
